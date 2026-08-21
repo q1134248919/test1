872a25a815c74a1452ffd8d1caded65a2a8f917a
quanjiahao <jiahao.quan@transsion.com>
2026-08-21 16:20:55 +0800

    fix: 修改bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://39.107.33.81:8080',
         changeOrigin: true,
       },
     },
